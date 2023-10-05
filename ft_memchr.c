7cd6ccd46bf875d815136f160feb749eef1e0e2a
Yasser Shalash <yasservanshalash@gmail.com>
2023-10-05 12:10:35 +0200

    added ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -15,13 +15,15 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
+	const unsigned char *unsigned_s;
 
 	i = 0;
+	unsigned_s = s;
 	while (i < n)
 	{
-		if (s[i] == c)
+		if (unsigned_s[i] == c)
 		{
-			return (&s[i]);
+			return (void *)(unsigned_s + i);
 		}
 		i++;
 	}
