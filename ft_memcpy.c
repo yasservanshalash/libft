a113e80512ab21f515bd2b53e6ff690a62c370b1
Yasser Shalash <yasservanshalash@gmail.com>
2023-10-06 20:15:09 +0200

    done with ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -12,24 +12,19 @@
 
 #include <stdio.h>
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char	*srcc;
-	char	*dstc;
-	size_t	i;
+	size_t i;
 
-	i = -1;
-	srcc = (char *)src;
-	dstc = (char *)dst;
-	if (dst == NULL || src == NULL || len == 0)
+	i = 0;
+	if (dest == NULL || src == NULL || n == 0)
 	{
-		return (dst);
+		return (dest);
 	}
-	if (srcc < dstc)
-		while ((int)(--len) >= 0)
-			*(dstc + len) = *(srcc + len);
-	else
-		while (++i < len)
-			*(dstc + i) = *(srcc + i);
-	return (dst);
+	while (i < n)
+	{
+		((char *)dest)[i] = ((char *)src)[i];
+		i++;
+	}
+	return (dest);
 }
