4933c2947052366113be0f96ba78e3791ce96bc7
Yasser Shalash <yshalash@f0r1s15.codam.nl>
2023-10-09 15:48:53 +0200

    finished substr

--- ft_substr.c
+++ ft_substr.c
@@ -13,6 +13,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static unsigned int	ft_strlen(char const *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
@@ -22,6 +34,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = start;
 	j = 0;
 	substring = (char *)malloc(len);
+	if (start > ft_strlen(s))
+	{
+		substring[0] = '\0';
+		return (substring);
+	}
 	if (substring == NULL)
 	{
 		return (NULL);
