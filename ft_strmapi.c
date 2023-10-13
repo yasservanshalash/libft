60fd226c9357a5c9313de70b91b18daa0d5ba0a3
Yasser Shalash <yasservanshalash@gmail.com>
2023-10-13 22:24:21 +0200

    done with strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,44 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+static int	ft_strlen(char const *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*result;
+	int		i;
+
+	result = (char *)malloc(ft_strlen(s) + 1);
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	toUpper(unsigned int index, char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+int	main(void)
+{
+	// Write C code here
+	printf("%s", ft_strmapi("hello", toUpper));
+	return (0);
+}
